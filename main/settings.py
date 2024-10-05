f881df94e58e0bfd13b959ef9d5fbc43a1792986
Robert Devereux <robert@Roberts-MacBook-Pro-2018.local>
2024-10-05 18:59:44 +0100

    Correcting mistake in ALLOWED_HOSTS

--- main/settings.py
+++ main/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLOWED_HOSTS = ['basic2.onrender.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['basic5.onrender.com','localhost', '127.0.0.1']
 
 
 # Application definition
