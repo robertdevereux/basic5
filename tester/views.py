1dc48eb34e26fa5cd6672c5610692643189869ec
Robert Devereux <robert@Roberts-MacBook-Pro-2018.local>
2024-10-06 17:38:34 +0100

    Added a database connection tester

--- tester/views.py
+++ tester/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connections
+from django.db.utils import OperationalError
 
 def tester_home(request):
     return HttpResponse("This is the home page of the app tester")
 
 def p2(request):
     return HttpResponse("This is the second page of the app tester")
+
+# tester/views.py
+def check_db_connection(request):
+    db_conn = connections['default']
+    try:
+        db_conn.cursor()
+        return HttpResponse("Database connection successful!")
+    except OperationalError:
+        return HttpResponse("Database connection failed!")
